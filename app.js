596ee6c6ddaa509a58e01eb69fbceb63fa853c17
Lance Owen Merrill <Lanceom@hawaii.edu>
2018-10-25 14:47:07 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+var letterUsed = document.getElementById('letterUsed');
+var wordPrompt = document.getElementById('wordPrompt');
+var userMessage = document.getElementById('userMessage');
+
+
+var guessesLeft = document.getElementById('guessLeft');
+ 
+
+var totalGuesses = 10;
+
+if( 3 > 2) {
+    for(var i = 0; i < totalGuesses; i) {
+        totalGuesses = totalGuesses - 1;
+        console.log(totalGuesses);
+
+    }
+}
+
+
+
+
